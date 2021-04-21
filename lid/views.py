d24eba5604348195daca36a1ed1b6e2d94abcf61
Igor-987 <igor.kov@list.ru>
2021-04-21 13:07:41 +0800

    ***

--- lid/views.py
+++ lid/views.py
@@ -9,23 +9,23 @@ def index(request):
     Функция отображения для домашней страницы сайта.
     """
 
-    name = request.GET.get('fio')
+    fio = request.GET.get('fio')
     # sname = request.GET.get('fio')
     # lname = request.GET.get('fio')
     phone = request.GET.get('phone')
-    addr = request.GET.get('adr')
+    adr = request.GET.get('adr')
 
     token = "a47b1aee73b17dd7ba087adc76b6db067ca725e2"
     with open('secret_key.txt') as f:
         secret = f.read().strip()
     dadata = Dadata(token, secret)
-    result = dadata.clean("name", name)
+    res_fio = dadata.clean(name="name", source=fio)
+    name = res_fio['name']
+    sname = res_fio['patronymic']
+    lname = res_fio['surname']
 
-    name = result[name]
-    sname = result[patronymic]
-    lname = result[surname]
-
-
+    res_addr = dadata.clean(name="address", source=adr)
+    addr = res_addr['result']
 
     load = {'FIELDS[NAME]': name,
             'FIELDS[SECOND_NAME]': sname,
